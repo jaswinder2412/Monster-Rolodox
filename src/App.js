119d168571e83977325e006fdc7a028a8a07f117
Jaswinder <maanjaswinder2412@gmail.com>
2022-08-26 04:35:26 -0400

    Monster Fetched

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-   
-    </div>
-  );
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      prevmonster:[]
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users ,  prevmonster : users}));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input type="text" name="Search-text"  id="searchbar" placeholder="Search Monster" onKeyUp={(e)=>{
+          const serchmonster = e.target.value.toLocaleLowerCase();
+          console.log(serchmonster)
+          if(serchmonster !== ''){
+            this.setState({ monsters: this.state.prevmonster })
+            const searchedMonster =  this.state.monsters.filter((monsters)=>{
+              return monsters.name.toLocaleLowerCase().includes(serchmonster)
+            })
+            this.setState({ monsters: searchedMonster })
+          }
+          else {
+            this.setState({ monsters: this.state.prevmonster })
+          }
+          
+        }}/>
+
+        {this.state.monsters.map((users) => {
+          return <h1 key={users.id}>{users.name}</h1>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
